b73aae9b62afbe61aa88d2e573e0e44663650e5f
nogtk <taka.naoga@moneyforward.co.jp>
2023-11-04 12:04:04 +0900

    Use mvc

--- index.js
+++ index.js
@@ -1,44 +1,132 @@
-function handleFormSubmit() {
-  const inputElement = document.getElementById('task-input');
-  const inputValue = inputElement.value;
-
-  if (!inputValue.length > 0) {
-    alert('Please enter a task');
-    return;
+class TodoListModel {
+  constructor() {
+    this.idCounter = 0;
+    this.todos = new Map();
   }
 
-  const todosElement = document.getElementById('todos');
-  const todoElement = createTodoElement(inputValue);
-  todosElement.appendChild(todoElement);
+  addTodo(task) {
+    this.idCounter += 1;
+    this.todos.set(this.idCounter, {
+      id: this.idCounter,
+      task,
+      checked: false,
+    });
+    return this.idCounter
+  }
 
-  inputElement.value = '';
+  getTodo(id) {
+    return this.todos.get(id);
+  }
+
+  removeTodo(id) {
+    return this.todos.delete(id);
+  }
+
+  checkTodo(id, isCheck) {
+    const todo = this.todos.get(id);
+    todo.checked = isCheck;
+    return todo;
+  }
 }
 
-function createTodoElement(task) {
-  const todoElement = document.createElement('li');
-  const checkBoxElement = document.createElement('input');
-  checkBoxElement.type = 'checkbox';
-  checkBoxElement.onchange = function (e) {
-    const checked = e.target.checked;
-    if (checked) {
-      todoElement.className = `checked`;
-    } else {
-      todoElement.className = "";
-    }
-  };
+class View {
+  addTodo(todo) {
+    const todosElement = document.getElementById('todos');
+    const todoElement = this._createTodoElement(todo);
+    todosElement.appendChild(todoElement);
+  }
 
-  const labelElement = document.createElement('label'); 
-  labelElement.innerText = task;
+  check(id) {
+    const todoElement = document.getElementById(`todo-${id}`);
+    todoElement.className = 'checked';
+  }
 
-  const buttonElement = document.createElement('button');
-  buttonElement.innerText = 'Delete';
-  buttonElement.onclick = function () {
+  unCheck(id) {
+    const todoElement = document.getElementById(`todo-${id}`);
+    todoElement.className = '';
+  }
+
+  removeTodo(id) {
+    const todoElement = document.getElementById(`todo-${id}`);
     todoElement.remove();
-  };
+  }
 
-  todoElement.appendChild(checkBoxElement);
-  todoElement.appendChild(labelElement);
-  todoElement.appendChild(buttonElement);
+  resetTodo() {
+    const input = document.getElementById('task-input');
+    input.value = '';
+  }
 
-  return todoElement;
+  _createTodoElement(todo) {
+    const {id, task} = todo;
+    const todoElement = document.createElement('li');
+    todoElement.id = `todo-${id}`;
+    const checkBoxElement = document.createElement('input');
+    todoElement.appendChild(checkBoxElement);
+    const labelElement = document.createElement('label');
+    labelElement.innerText = task;
+    checkBoxElement.type = 'checkbox';
+    checkBoxElement.id = `checkbox-${id}`;
+    todoElement.appendChild(labelElement);
+
+    const buttonElement = document.createElement('button');
+    buttonElement.innerText = 'Delete';
+    buttonElement.id = `button-${id}`;
+    todoElement.appendChild(buttonElement);
+
+    return todoElement;
+  }
 }
+
+const todoList = new TodoListModel();
+const view = new View();
+
+class Controller {
+  setup() {
+    this.handleSubmitForm();
+  }
+
+  handleSubmitForm() {
+    const formElement = document.getElementById('task-send-form');
+  formElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const input = document.getElementById('task-input');
+      const task = input.value;
+      if (!task.length > 0) {
+        alert('Please enter a task');
+        return;
+      }
+
+      const addedTodoId = todoList.addTodo(task);
+      const todo = todoList.getTodo(addedTodoId);
+
+      view.addTodo(todo);
+      this.handleCheckTask(todo.id);
+      this.handleClickDeleteTask(todo.id);
+      view.resetTodo();
+    });
+  }
+
+  handleCheckTask(id) {
+    const checkBoxElement = document.getElementById(`checkbox-${id}`);
+    checkBoxElement.onchange = function (e) {
+      const checked = e.target.checked;
+      const stateChangedTodo = todoList.checkTodo(id, checked);
+      if (stateChangedTodo.checked) {
+        view.check(stateChangedTodo.id);
+      } else {
+        view.unCheck(stateChangedTodo.id);
+      }
+    };
+  }
+
+  handleClickDeleteTask(id) {
+    const buttonElement = document.getElementById(`button-${id}`);
+    buttonElement.onclick = function () {
+      view.removeTodo(id);
+      todoList.removeTodo(id);
+    };
+  }
+}
+
+const formController = new Controller();
+formController.setup();
